be9f7a58f412840c03424793b16f6750ec6369b6
ryumg0523 <162393912+2403111003@users.noreply.github.com>
2024-03-06 18:21:46 +0900

    Add files via upload

---
+++ Print01.java
@@ -0,0 +1,18 @@
+package sec04;
+
+public class Print01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String myName = "홍길동";
+		 int myAge = 20;
+		 
+		 System.out.println("이름 : " + myName);
+		 System.out.println("나이 : " + myAge);
+		 System.out.println();
+		 System.out.print("이름 : " + myName);
+		 System.out.println("  나이 : " + myAge);
+		 
+		 }
+
+}
